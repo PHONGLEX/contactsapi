76c9df6f5cfaead0dba76e0d2ecb7cc194ceaecb
phonglex <fonglex@gmail.com>
2021-05-23 16:20:47 +0700

    add static config

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 import environ
 env = environ.Env(
@@ -132,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
